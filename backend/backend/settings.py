045e2aad0581913720d1da668772000b9728e542
GanpathChoyal <ganpath9941899@gmail.com>
2025-10-02 21:13:08 +0530

    url change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,8 +27,13 @@ import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = [
+    "majestic-stroopwafel-ba92c0.netlify.app",
+    "your-backend.onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "https://majestic-stroopwafel-ba92c0.netlify.app").split(",")
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
