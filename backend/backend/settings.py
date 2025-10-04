407e44d3ac6386d8190cd450d36170f4d008deff
GanpathChoyal <ganpath9941899@gmail.com>
2025-10-04 22:17:10 +0530

    url change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     "bright-meerkat-6a6400.netlify.app",
-    "your-backend.onrender.com",
+    "react-pythonfullstack.onrender.com",
     "127.0.0.1",
     "localhost",
 ]
